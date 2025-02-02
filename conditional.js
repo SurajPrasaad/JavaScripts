b1dab26ca1c1b507bda14226dabb3187e37bfef8
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-02-02 23:41:15 +0530

    conditional.js file added

---
+++ conditional.js
@@ -0,0 +1,14 @@
+let numberOfGuest = 10;
+// small - <= 2
+// medium - <= 4
+// large
+let pizza;
+if(numberOfGuest<=2){
+    pizza = "small"
+}else if(numberOfGuest<=4){
+    pizza = "medium"
+}else{
+    pizza = "large"
+}
+
+console.log(`Bhai Customers ki janshnaya ${numberOfGuest} hai! , so pizza inko ${pizza} chaiye😂`)
