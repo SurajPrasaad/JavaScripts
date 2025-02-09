c005f1ed660963e368fc0d039e9a3e0b8c732790
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-02-09 22:59:49 +0530

    js code challenge notes objects and polifils

---
+++ Polifils/pop.js
@@ -0,0 +1,12 @@
+if(!Array.prototype.myPop){
+    Array.prototype.myPop = function(){
+       let lastElement = this[this.length-1];
+       this.length = this.length-1;
+        return lastElement;
+
+    }
+}
+
+let arr = [1,2,3];
+console.log(arr.myPop());
+console.log(arr)
---
+++ Polifils/push.js
@@ -0,0 +1,15 @@
+if(!Array.prototype.myPush){
+    Array.prototype.myPush = function(...elements){
+        let length = this.length
+        for(let i = 0;i<elements.length;i++){
+            this[length] = elements[i];
+            length++;
+        }
+        return this.length
+    }
+}
+
+let arr = [1,2,3];
+arr.p
+arr.myPush(60)
+console.log(arr)
