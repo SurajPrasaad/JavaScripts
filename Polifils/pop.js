1363dae4dbe4d3adee362c5b0e8c6fbcbfb20a5a
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-02-10 20:57:43 +0530

    pop  method in array polifils

--- Polifils/pop.js
+++ Polifils/pop.js
@@ -1,12 +1,11 @@
-if(!Array.prototype.myPop){
-    Array.prototype.myPop = function(){
-       let lastElement = this[this.length-1];
-       this.length = this.length-1;
-        return lastElement;
-
-    }
+if (!Array.prototype.myPop) {
+  Array.prototype.myPop = function () {
+    let lastElement = this[this.length - 1];
+    this.length = this.length - 1;
+    return lastElement;
+  };
 }
 
-let arr = [1,2,3];
+let arr = [1, 2, 3];
 console.log(arr.myPop());
-console.log(arr)
+console.log(arr);
