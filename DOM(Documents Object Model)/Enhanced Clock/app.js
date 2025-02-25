1a1d3db1458d7aaec146ad4f6999311d9ece0e78
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-02-25 20:57:20 +0530

    Updated:Enhanced Clock Project

--- DOM(Documents Object Model)/Enhanced Clock/app.js
+++ DOM(Documents Object Model)/Enhanced Clock/app.js
@@ -1,23 +1,35 @@
 let digitalClock = document.querySelector('.digital-clock');
 let dateText = document.querySelector('.date');
 
+let hourHand = document.querySelector('.hand.hour');
+let minuteHand = document.querySelector('.hand.minute');
+let secondHand = document.querySelector('.hand.second');
 
 let updateClock = () => {
     let date = new Date();
-    let hours = (date.getHours() % 12).toString().padStart(2, "0");
+    let hours = (date.getHours() % 12 || 12).toString().padStart(2, "0");
     let minutes = date.getMinutes().toString().padStart(2, "0");
-    let ampm = hours > 12 ? "AM" : "PM";
-    let seconds = date.getSeconds().toString().padStart("2", 0)
-    digitalClock.textContent = `${hours}:${minutes}:${seconds}:${ampm}`;
+    let seconds = date.getSeconds().toString().padStart(2, "0");
+    let ampm = date.getHours() >= 12 ? "PM" : "AM";
+    digitalClock.textContent = `${hours}:${minutes}:${seconds} ${ampm}`;
+    
     const options = {
         weekday: "long",
         year: "numeric",
         month: "long",
         day: "numeric",
     };
-    let currentDate = date.toLocaleDateString(undefined, options)
-    dateText.textContent = `${currentDate}`
+    let currentDate = date.toLocaleDateString(undefined, options);
+    dateText.textContent = `${currentDate}`;
 
+    let hourRotation = (hours % 12) * 30 + minutes * 0.5;
+    let minuteRotation = minutes * 6 + seconds * 0.1;
+    let secondRotation = seconds * 6;
+
+    hourHand.style.transform = `rotate(${hourRotation}deg)`;
+    minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
+    secondHand.style.transform = `rotate(${secondRotation}deg)`;
 }
-setInterval(updateClock, 1000)
-updateClock()
+
+setInterval(updateClock, 1000);
+updateClock();
